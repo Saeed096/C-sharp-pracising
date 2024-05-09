3bfb6b176d1d05a9681472cb561a647c9ae4ddb6
Saeed Mohamed <132412767+Saeed096@users.noreply.github.com>
2024-05-09 14:03:01 +0300

    Initial commit

---
+++ Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning
+{
+    internal static class Extensions
+    {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> items, Predicate<T> predicate)  
+        {
+
+            foreach (T item in items) 
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+
+        }
+        IEnumerable
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, -3, 4, -5, 6, 7, -8, 9 };
+
+/*trying to iterate >> even nums "foreach" >> iterate one item for resule then return implement even num for each on it and so on????? */
+
+
+            IEnumerable<int> result = list.Filter(x => x > 0);      
+            IEnumerable<int> evenNumbers = result.Filter(x => x % 2 == 0); 
+
+            foreach (var item in evenNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
